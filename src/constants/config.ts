b7e651d51a87aece53a76dc502ef3d804a211a9c
tien nguyen <kudoshinichik50@gmail.com>
2026-03-16 01:59:49 +0700

    Move baseUrl

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,6 +1,5 @@
 export const config = {
-    API_BASE_URL:
-        import.meta.env.VITE_API_BASE_URL ?? 'https://dev.api.tichtich.vn',
+    API_BASE_URL: import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:3000',
     APP_NAME: import.meta.env.VITE_APP_NAME ?? 'TichTichApp',
     APP_VERSION: import.meta.env.VITE_APP_VERSION ?? '1.0.0',
     IS_DEV: import.meta.env.MODE === 'development',

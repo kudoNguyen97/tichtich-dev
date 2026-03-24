8feecd2b3e37ea887ac9780ada37f72bf97dddf1
Tien Nguyen <kudonguyen12485@gmail.com>
2026-03-24 17:15:51 +0700

    Update firebaseConfig

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -8,20 +8,13 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    // apiKey: 'AIzaSyCL6B0O4yinKVHzp15QJivGYvgUc_z85uM',
-    // authDomain: 'tichtich-kudo.firebaseapp.com',
-    // projectId: 'tichtich-kudo',
-    // storageBucket: 'tichtich-kudo.firebasestorage.app',
-    // messagingSenderId: '839764689523',
-    // appId: '1:839764689523:web:33d5b4f33b5f13c7776006',
-    // measurementId: 'G-LYMPWV1C1N',
-    apiKey: 'AIzaSyCvTimXRqla_whza5xuRVSSOnAPbWRXm_s',
-    authDomain: 'tichtichvn-dev.firebaseapp.com',
-    projectId: 'tichtichvn-dev',
-    storageBucket: 'tichtichvn-dev.firebasestorage.app',
-    messagingSenderId: '464983822805',
-    appId: '1:464983822805:web:c4fb943d783850f9192d5c',
-    measurementId: 'G-H1W6LNLJMG',
+    apiKey: import.meta.env.FIREBASE_API_KEY,
+    authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.FIREBASE_APP_ID,
+    measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase

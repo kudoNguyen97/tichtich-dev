82edd162b9ada05ef7982d1147f16a6ecf309c47
tien nguyen <kudoshinichik50@gmail.com>
2026-04-28 02:33:17 +0700

    Update path icons

--- src/constants/children/character/itemVisuals.ts
+++ src/constants/children/character/itemVisuals.ts
@@ -52,72 +52,119 @@ export const FALLBACK_BY_SLOT: Record<string, Visual> = {
     fashion_set: { emoji: '💎', color: '#9C27B0' },
 };
 
+// const IMAGE_BY_SLUG: Partial<Record<string, string>> = {
+//     clothing_top_ao_den_uwu:
+//         '/src/assets/icons/items/clothings/shirt-top/uwu.svg?react',
+//     clothing_top_ao_graffiti:
+//         '/src/assets/icons/items/clothings/shirt-top/graffiti.svg?react',
+//     clothing_top_ao_soc_thuy_thu:
+//         '/src/assets/icons/items/clothings/shirt-top/sailor.svg?react',
+//     clothing_top_ao_hoodie_xanh:
+//         '/src/assets/icons/items/clothings/shirt-top/hoodie-blue.svg?react',
+//     clothing_top_ao_vang_mua_he:
+//         '/src/assets/icons/items/clothings/shirt-top/yellow-summer.svg?react',
+//     clothing_top_ao_yem_jeans:
+//         '/src/assets/icons/items/clothings/shirt-top/jeans.svg?react',
+//     clothing_bottom_vay_pickleball:
+//         '/src/assets/icons/items/clothings/trousers/pickleball.svg?react',
+//     clothing_bottom_vay_jean_ngan:
+//         '/src/assets/icons/items/clothings/trousers/short-jeans.svg?react',
+//     clothing_bottom_vay_xoe_hong:
+//         '/src/assets/icons/items/clothings/trousers/flared-pink.svg?react',
+//     clothing_bottom_quan_keo_xanh:
+//         '/src/assets/icons/items/clothings/trousers/blue-candy.svg?react',
+//     clothing_bottom_quan_ran_ri:
+//         '/src/assets/icons/items/clothings/trousers/ranri.svg?react',
+//     clothing_bottom_quan_xanh_mua_he:
+//         '/src/assets/icons/items/clothings/trousers/cold-blue.svg?react',
+//     clothing_bottom_quan_den_ngau:
+//         '/src/assets/icons/items/clothings/trousers/cool-black.svg?react',
+//     shoes_giay_dong_xu: '/src/assets/icons/items/shoes/dimple.svg?react',
+//     shoes_giay_bale: '/src/assets/icons/items/shoes/bale.svg?react',
+//     shoes_giay_bata_vang: '/src/assets/icons/items/shoes/yellow-bata.svg?react',
+//     shoes_giay_boot_ngau: '/src/assets/icons/items/shoes/cool-boot.svg?react',
+//     shoes_giay_converse_cam: '/src/assets/icons/items/shoes/converse.svg?react',
+//     accessories_hat_non_dau:
+//         '/src/assets/icons/items/accessory/hat/strawberry.svg?react',
+//     accessories_hat_non_tai_meo:
+//         '/src/assets/icons/items/accessory/hat/cat-ear.svg?react',
+//     accessories_eyewear_kinh_tron:
+//         '/src/assets/icons/items/accessory/glasses/circle.svg?react',
+//     accessories_necklace_day_chuyen_trai_tim:
+//         '/src/assets/icons/items/accessory/necklace/heart.svg?react',
+//     accessories_bracelet_dong_ho_thong_minh:
+//         '/src/assets/icons/items/accessory/bracelet/clock.svg?react',
+//     fashion_set_chi_hang:
+//         '/src/assets/icons/items/fashion-set/sister-hang.svg?react',
+//     fashion_set_chu_cuoi:
+//         '/src/assets/icons/items/fashion-set/uncle-cuoi.svg?react',
+//     fashion_set_ao_dai_do:
+//         '/src/assets/icons/items/fashion-set/red-ao-dai.svg?react',
+//     fashion_set_vay_noel_nu:
+//         '/src/assets/icons/items/fashion-set/noel-girl.svg?react',
+//     fashion_set_noel_nam:
+//         '/src/assets/icons/items/fashion-set/noel-boy.svg?react',
+//     fashion_set_vay_xinh_sinh_nhat:
+//         '/src/assets/icons/items/fashion-set/birthday.svg?react',
+//     fashion_set_ao_dau_lan:
+//         '/src/assets/icons/items/fashion-set/unicorn-head.svg?react',
+//     fashion_set_harry_potter:
+//         '/src/assets/icons/items/fashion-set/harry-potter.svg?react',
+//     fashion_set_heo_superman:
+//         '/src/assets/icons/items/fashion-set/superman.svg?react',
+//     fashion_set_heo_wonder_woman:
+//         '/src/assets/icons/items/fashion-set/wonder-women.svg?react',
+//     fashion_set_hiep_si_vang:
+//         '/src/assets/icons/items/fashion-set/golden-knight.svg?react',
+//     fashion_set_hiep_si_xanh:
+//         '/src/assets/icons/items/fashion-set/blue-knight.svg?react',
+// };
 const IMAGE_BY_SLUG: Partial<Record<string, string>> = {
-    clothing_top_ao_den_uwu:
-        '/src/assets/icons/items/clothings/shirt-top/uwu.svg?react',
-    clothing_top_ao_graffiti:
-        '/src/assets/icons/items/clothings/shirt-top/graffiti.svg?react',
-    clothing_top_ao_soc_thuy_thu:
-        '/src/assets/icons/items/clothings/shirt-top/sailor.svg?react',
+    clothing_top_ao_den_uwu: '/icons/items/clothings/shirt-top/uwu.svg',
+    clothing_top_ao_graffiti: '/icons/items/clothings/shirt-top/graffiti.svg',
+    clothing_top_ao_soc_thuy_thu: '/icons/items/clothings/shirt-top/sailor.svg',
     clothing_top_ao_hoodie_xanh:
-        '/src/assets/icons/items/clothings/shirt-top/hoodie-blue.svg?react',
+        '/icons/items/clothings/shirt-top/hoodie-blue.svg',
     clothing_top_ao_vang_mua_he:
-        '/src/assets/icons/items/clothings/shirt-top/yellow-summer.svg?react',
-    clothing_top_ao_yem_jeans:
-        '/src/assets/icons/items/clothings/shirt-top/jeans.svg?react',
+        '/icons/items/clothings/shirt-top/yellow-summer.svg',
+    clothing_top_ao_yem_jeans: '/icons/items/clothings/shirt-top/jeans.svg',
     clothing_bottom_vay_pickleball:
-        '/src/assets/icons/items/clothings/trousers/pickleball.svg?react',
+        '/icons/items/clothings/trousers/pickleball.svg',
     clothing_bottom_vay_jean_ngan:
-        '/src/assets/icons/items/clothings/trousers/short-jeans.svg?react',
+        '/icons/items/clothings/trousers/short-jeans.svg',
     clothing_bottom_vay_xoe_hong:
-        '/src/assets/icons/items/clothings/trousers/flared-pink.svg?react',
+        '/icons/items/clothings/trousers/flared-pink.svg',
     clothing_bottom_quan_keo_xanh:
-        '/src/assets/icons/items/clothings/trousers/blue-candy.svg?react',
-    clothing_bottom_quan_ran_ri:
-        '/src/assets/icons/items/clothings/trousers/ranri.svg?react',
+        '/icons/items/clothings/trousers/blue-candy.svg',
+    clothing_bottom_quan_ran_ri: '/icons/items/clothings/trousers/ranri.svg',
     clothing_bottom_quan_xanh_mua_he:
-        '/src/assets/icons/items/clothings/trousers/cold-blue.svg?react',
+        '/icons/items/clothings/trousers/cold-blue.svg',
     clothing_bottom_quan_den_ngau:
-        '/src/assets/icons/items/clothings/trousers/cool-black.svg?react',
-    shoes_giay_dong_xu: '/src/assets/icons/items/shoes/dimple.svg?react',
-    shoes_giay_bale: '/src/assets/icons/items/shoes/bale.svg?react',
-    shoes_giay_bata_vang: '/src/assets/icons/items/shoes/yellow-bata.svg?react',
-    shoes_giay_boot_ngau: '/src/assets/icons/items/shoes/cool-boot.svg?react',
-    shoes_giay_converse_cam: '/src/assets/icons/items/shoes/converse.svg?react',
-    accessories_hat_non_dau:
-        '/src/assets/icons/items/accessory/hat/strawberry.svg?react',
-    accessories_hat_non_tai_meo:
-        '/src/assets/icons/items/accessory/hat/cat-ear.svg?react',
-    accessories_eyewear_kinh_tron:
-        '/src/assets/icons/items/accessory/glasses/circle.svg?react',
+        '/icons/items/clothings/trousers/cool-black.svg',
+    shoes_giay_dong_xu: '/icons/items/shoes/dimple.svg',
+    shoes_giay_bale: '/icons/items/shoes/bale.svg',
+    shoes_giay_bata_vang: '/icons/items/shoes/yellow-bata.svg',
+    shoes_giay_boot_ngau: '/icons/items/shoes/cool-boot.svg',
+    shoes_giay_converse_cam: '/icons/items/shoes/converse.svg',
+    accessories_hat_non_dau: '/icons/items/accessory/hat/strawberry.svg',
+    accessories_hat_non_tai_meo: '/icons/items/accessory/hat/cat-ear.svg',
+    accessories_eyewear_kinh_tron: '/icons/items/accessory/glasses/circle.svg',
     accessories_necklace_day_chuyen_trai_tim:
-        '/src/assets/icons/items/accessory/necklace/heart.svg?react',
+        '/icons/items/accessory/necklace/heart.svg',
     accessories_bracelet_dong_ho_thong_minh:
-        '/src/assets/icons/items/accessory/bracelet/clock.svg?react',
-    fashion_set_chi_hang:
-        '/src/assets/icons/items/fashion-set/sister-hang.svg?react',
-    fashion_set_chu_cuoi:
-        '/src/assets/icons/items/fashion-set/uncle-cuoi.svg?react',
-    fashion_set_ao_dai_do:
-        '/src/assets/icons/items/fashion-set/red-ao-dai.svg?react',
-    fashion_set_vay_noel_nu:
-        '/src/assets/icons/items/fashion-set/noel-girl.svg?react',
-    fashion_set_noel_nam:
-        '/src/assets/icons/items/fashion-set/noel-boy.svg?react',
-    fashion_set_vay_xinh_sinh_nhat:
-        '/src/assets/icons/items/fashion-set/birthday.svg?react',
-    fashion_set_ao_dau_lan:
-        '/src/assets/icons/items/fashion-set/unicorn-head.svg?react',
-    fashion_set_harry_potter:
-        '/src/assets/icons/items/fashion-set/harry-potter.svg?react',
-    fashion_set_heo_superman:
-        '/src/assets/icons/items/fashion-set/superman.svg?react',
-    fashion_set_heo_wonder_woman:
-        '/src/assets/icons/items/fashion-set/wonder-women.svg?react',
-    fashion_set_hiep_si_vang:
-        '/src/assets/icons/items/fashion-set/golden-knight.svg?react',
-    fashion_set_hiep_si_xanh:
-        '/src/assets/icons/items/fashion-set/blue-knight.svg?react',
+        '/icons/items/accessory/bracelet/clock.svg',
+    fashion_set_chi_hang: '/icons/items/fashion-set/sister-hang.svg',
+    fashion_set_chu_cuoi: '/icons/items/fashion-set/uncle-cuoi.svg',
+    fashion_set_ao_dai_do: '/icons/items/fashion-set/red-ao-dai.svg',
+    fashion_set_vay_noel_nu: '/icons/items/fashion-set/noel-girl.svg',
+    fashion_set_noel_nam: '/icons/items/fashion-set/noel-boy.svg',
+    fashion_set_vay_xinh_sinh_nhat: '/icons/items/fashion-set/birthday.svg',
+    fashion_set_ao_dau_lan: '/icons/items/fashion-set/unicorn-head.svg',
+    fashion_set_harry_potter: '/icons/items/fashion-set/harry-potter.svg',
+    fashion_set_heo_superman: '/icons/items/fashion-set/superman.svg',
+    fashion_set_heo_wonder_woman: '/icons/items/fashion-set/wonder-women.svg',
+    fashion_set_hiep_si_vang: '/icons/items/fashion-set/golden-knight.svg',
+    fashion_set_hiep_si_xanh: '/icons/items/fashion-set/blue-knight.svg',
 };
 
 export function getVisual(
